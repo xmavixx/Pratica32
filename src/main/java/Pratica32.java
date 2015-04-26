575c3780120ec6081719c344e48942e82bd8fa86
xmavixx <vitholken@hoptmail.com>
2015-04-26 18:58:45 -0300

    Update Pratica32.java

--- src/main/java/Pratica32.java
+++ src/main/java/Pratica32.java
@@ -1,3 +1,4 @@
+import static java.lang.Math.*;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,13 +10,16 @@
  * @author Victor
  */
 public class Pratica32 {
+    public static void main(String[] args) {
+        /* Dentro do método main faça uma chamada para a função densidade, 
+        passando valores para x, media e desvio de -1, 67 e 3 respectivamente. 
+        Exiba o valor calculado. Verifique se o valor calculado está correto.*/
+        System.out.println(densidade(-1, 67, 3));
+    }
     
-    private static double d;
-
-    public static double densidade(double x, double media, double desvio) {
-
-        d = (1 / ((((Math.sqrt(2 * Math.PI)))*desvio))) * Math.pow(Math.E, (-1 / 2) * Math.pow(((x - media) / desvio), 2));
-
+    public static double densidade(double x, double media, 
+            double desvio) {
+        double d = (exp(-0.5*(pow((x - media)/desvio, 2))))/(sqrt(2*PI)*desvio);
         return d;
     }
 }
